5f2e3aef588063593c3c24e305fd98790f5dbbe7
jayinnhahn <jaytan3825@gmail.com>
2023-10-10 19:30:58 +0800

    added useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
 import Post from './components/Post'
+import { useEffect, useState } from 'react'
 function App() {
+  const [number, setNumber] = useState(1);
+  const [post, setPost] = useState<{
+    body: string;
+    id: number;
+    title: string;
+    userId: Number;
+  } | null>(null);
+  
+  // const fetchPost = async (id) => {
+  //   try {    
+  //   const response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+  //   const data = await response.json()
 
+  //   setPost(data)
+
+  //   } catch(error) {
+
+  //   }
+  //   finally(){
+  //     console.log("done")
+  //   }
+  // }
+
+
+
+  useEffect(() => {
+    if (number){
+      fetch('https://jsonplaceholder.typicode.com/posts/' + number)
+      .then((response) => response.json())
+      .then((data) => setPost(data))
+    }
+
+  }, [number])
+   
   return (
 		<div className="flex justify-center w-screen items-center h-screen bg-gray-100">
       <div> 
-        <Post/>
+      {number}
+      {post && <Post title={post.title} body={post.body}/>}
           <div className='flex items-center gap-3 mt-3'> 
               {Array.from({length: 5}).map((value, index) => {
                 return(
-                  <button className="p-2 shadow-md rounded-md text-center font-bold w-full bg-blue-700 mb-2 text-white hover:cursor-pointer"> {index + 1} </button>
+                  <button className="p-2 shadow-md rounded-md text-center font-bold w-full bg-blue-700 mb-2 text-white hover:cursor-pointer" onClick={()=> setNumber(index + 1)} > {index + 1} </button>
                 )
               })}
           </div>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,11 +1,18 @@
 
-const Post = () => {
+import React from "react";
+
+interface PostProps {
+	title: string;
+	body: string;
+}
+
+const Post: React.FC<PostProps> = ({title, body}) => {
   return (
     <div className="p-4 bg-white border-2 rounded-lg shadow-md p-8 text-center">
-        <h4> Title </h4>
-        <p> Body </p>
+        <h4 className="font-bold text-[2.5rem] text-left"> {title} </h4>
+        <p className="font-light text-[1rem] w-auto flex flex-wrap text-justify"> {body} </p>
     </div>
-  )
-}
+  );
+};
 
 export default Post
